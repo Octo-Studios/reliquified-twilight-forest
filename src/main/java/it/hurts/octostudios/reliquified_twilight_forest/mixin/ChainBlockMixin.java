e00bbd788018ae93adf113641931427a5f779cc3
catboybinary <catboybinary@disroot.org>
2025-03-14 03:19:49 +0300

    forgot stun

--- src/main/java/it/hurts/octostudios/reliquified_twilight_forest/mixin/ChainBlockMixin.java
+++ src/main/java/it/hurts/octostudios/reliquified_twilight_forest/mixin/ChainBlockMixin.java
@@ -1,8 +1,11 @@
 package it.hurts.octostudios.reliquified_twilight_forest.mixin;
 
 import it.hurts.octostudios.reliquified_twilight_forest.item.relic.SteelCapeItem;
+import it.hurts.sskirillss.relics.init.EffectRegistry;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
@@ -25,6 +28,9 @@ public abstract class ChainBlockMixin {
         ChainBlock self = (ChainBlock) (Object) this;
         if (instance.hurt(source, (float) relic.getStatValue(stack, "iron_guard", "damage"))) {
             self.playSound(TFSounds.BLOCK_AND_CHAIN_HIT.get(), 1.0f, self.getRandom().nextFloat());
+            if (instance instanceof LivingEntity living) {
+                living.addEffect(new MobEffectInstance(EffectRegistry.STUN, (int) relic.getStatValue(stack, "iron_guard", "stun_duration"), 0, false, true, true));
+            }
             return false;
         }
 
